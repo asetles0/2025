2e166d78a03c69cdcfb696e660a59015a60e61e5
Aset <asetles0@gmail.com>
2024-11-11 10:28:17 +0500

    first commit

---
+++ 1 js/js.js
@@ -0,0 +1,19 @@
+let num = prompt("1-ден 12-дейін сан енгізіңіз:");
+
+if (num % 2 == 0) {
+  alert("Ай 1, Маусым Scring");
+} else {
+  alert("Ай 1, Тамыз Scring");
+}
+
+if (num % 2 == 0) {
+  alert("Ай 1, Маусым Scring");
+} else {
+  alert("Ай 1, Тамыз Scring");
+}
+
+if (num % 2 == 0) {
+  alert("Ай 1, Маусым Scring");
+} else {
+  alert("Ай 1, Тамыз Scring");
+}
